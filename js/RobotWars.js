8894e8ed4b86d07fb2097ca0ea77eaec0ad6e7b9
Matt Keas <matthiasak@gmail.com>
2014-11-20 13:00:35 -0600

    Day 2 group day

--- js/RobotWars.js
+++ js/RobotWars.js
@@ -19,10 +19,16 @@ var Robot = Backbone.Model.extend({
     urlRoot:"http://space-robots.herokuapp.com/robots",
     defaults: {
         name: "Bender",
-        photo: "https://31.media.tumblr.com/tumblr_m4aogmpvKP1qdnki4o1_400.gif"
+        photo: "https://31.media.tumblr.com/tumblr_m4aogmpvKP1qdnki4o1_400.gif",
+        shouldRenderToScreen: true
     },
     initialize: function(){
-        this.view = new RobotView({ model: this })
+        if(this.get('shouldRenderToScreen')){
+            this.view = new RobotView({ model: this })
+        }
+    },
+    parse: function(data){
+        return data.robot;
     }
 })
 
@@ -53,31 +59,35 @@ var MissionView = Backbone.View.extend({
 })
 
 var Mission = Backbone.Model.extend({
+    urlRoot: "http://space-robots.herokuapp.com/missions",
     defaults: {
         song: "Ice Ice Baby",
         dance: "Foxtrot",
         location: "pluto",
         completed: false
     },
-    validate: function(attrs){
-        if(typeof attrs.robot_id === "undefined"){
-            return "No robot id provided.";
-        }
-    },
+    // validate: function(attrs){
+    //     if(typeof attrs.robot_id === "undefined" || (typeof attrs.robot_id !== "number" && !attrs.robot_id)){
+    //         alert("Y U NO ROBOT ID");
+    //         return "No robot id provided.";
+    //     }
+    // },
     initialize: function(){
         this.view = new MissionView({ model: this })
 
         var location = new Giphy({ query: this.get('location') });
         var song = new Giphy({ query: this.get('song') });
         var dance = new Giphy({ query: this.get('dance') });
+        var robot = new Robot({ id: this.get('robot_id'), shouldRenderToScreen: false });
 
         var self = this;
         $.when(
             location.fetch(),
             song.fetch(),
-            dance.fetch()
+            dance.fetch(),
+            robot.fetch()
         ).then(function(){
-
+            self.set('robot_photo', robot.get('photo'));
             self.set('giphy_location_url', location.get('url'));
             self.set('giphy_song_url', song.get('url'));
             self.set('giphy_dance_url', dance.get('url'));
@@ -91,12 +101,14 @@ var MissionCollection = Backbone.Collection.extend({
     model: Mission,
     url: "http://space-robots.herokuapp.com/missions",
     initialize: function(){
-        this.fetch().then(function(){
-
-        })
+        this.startRequest()
     },
     parse: function(data){
         return data.missions
+    },
+    startRequest: function(){
+        document.querySelector('.mission-container').innerHTML = "";
+        this.fetch();
     }
 })
 
@@ -122,11 +134,66 @@ var Giphy = Backbone.Model.extend({
     }
 })
 
-var testGiphy = new Giphy({ query: "explosion" })
+function TURNARRAYRIGHTNOW(arr){
+    /// TURN ARRAY
+    ///
+    /// RIIIIIIIGHT NAOW
+    var obj = {};
+    for(var i in arr){
+        obj[arr[i].name] = arr[i].value
+    };
+    return obj;
+}
 
-// window.testGiphy = testGiphy;
+var MissionCreator = Backbone.View.extend({
+    el: document.querySelector('.mission-creator'),
+    events: {
+        "submit form": "addMission"
+    },
+    addMission: function(event){
+        event.preventDefault();
 
-// window.robots = new RobotCollective();
+        var arr = this.$el.find('input').serializeArray();
+        var obj = TURNARRAYRIGHTNOW(arr)
+
+        var newMission = new Mission(obj);
+        var status = newMission.save();
+
+        status.then(function(){
+            window.missions.startRequest();
+        })
+
+        status.fail(function(data){
+            alert("ERROR:"+JSON.stringify(data));
+        })
+    }
+})
+
+var AppView = Backbone.View.extend({
+    el: document.body,
+    initialize: function(){
+        this.views = {
+            robots: $('.robots-container'),
+            missions: $('.missions, .mission-creator')
+        }
+    },
+    events: {
+        "click .mission-control a": "handleClick",
+    },
+    handleClick: function(event){
+        var clicked = event.target;
+        var show = clicked.getAttribute('show');
+        var self = this;
+        _.forEach(this.views, function(value, key){
+            value.fadeOut();
+        })
+        this.views[show].fadeIn();
+    }
+})
+
+window.robots = new RobotCollective();
 window.missions = new MissionCollection();
+window.missionCreator = new MissionCreator();
+window.appView = new AppView();
 
 })(window, undefined)
